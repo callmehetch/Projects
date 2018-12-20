16c0ff9a5d0cd512b42218f71a2e77b71733cc42
callmehetch <callmehetch@gmail.com>
2018-12-20 18:43:16 +0530

    Surprise Commit

---
+++ birthday/declare.php
@@ -0,0 +1,9 @@
+<?php
+
+$mailhost = 'smtp.gmail.com';
+$mailusername = '54321hemanth@gmail.com';
+$mailpass = 'hemanth3219';
+$mailsmtp = 'tls';
+$mailport = '587';
+
+?>
---
+++ birthday/success.php
@@ -0,0 +1,43 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <title>IIITDM-Form</title>
+    <style>
+      
+
+      </style>
+  </head>
+    <body>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body></html>
